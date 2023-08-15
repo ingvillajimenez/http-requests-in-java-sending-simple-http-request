7c8919714be66396e72b8d38128e2f32d3abceaf
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-15 14:03:08 -0600

    PUT and DELETE Requests

--- httprequests/src/main/java/com/loonycorn/SimpleHttpURLConnection.java
+++ httprequests/src/main/java/com/loonycorn/SimpleHttpURLConnection.java
@@ -18,50 +18,76 @@ public class SimpleHttpURLConnection {
 
         try {
 
-            URL url = new URL("https://reqres.in/api/users");
+            URL url = new URL("https://reqres.in/api/users/277");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            conn.setRequestMethod("POST");
+            conn.setRequestMethod("DELETE");
+//            conn.setRequestMethod("PUT");
 
-            conn.setDoOutput(true);
-
-            String postData = "{'email':'alice@loonycorn.com'," +
-                    "'firstName':'Alice','lastName':'Alison'}";
-
-            try (OutputStream outputStream = conn.getOutputStream()) {
-
-                byte[] postBytes = postData.getBytes("utf-8");
-                outputStream.write(postBytes, 0, postBytes.length);
-            }
+//            conn.setDoOutput(true);
+//
+//            String postData = "{'email':'alice.alison@loonycorn.com'," +
+//                    "'firstName':'Alice','lastName':'Alison'}";
+//
+//            try (OutputStream outputStream = conn.getOutputStream()) {
+//
+//                byte[] postBytes = postData.getBytes("utf-8");
+//                outputStream.write(postBytes, 0, postBytes.length);
+//            }
 
             System.out.println("Response code: " + conn.getResponseCode());
-            //Response code: 201
+            //Response code: 204
+            //Response code: 204
+            //Response code: 200
             System.out.println("Response message: " + conn.getResponseMessage());
-            //Response message: Created
+            //Response message: No Content
+            //Response message: No Content
+            //Response message: OK
 
-            try (BufferedReader read = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+            long responseSize = conn.getContentLengthLong();
+            System.out.println("Size: " + responseSize);
+            //Size: 0
 
-                StringBuilder responseText = new StringBuilder();
+            if (responseSize > 0) {
 
-                while ((text = read.readLine()) != null) {
-                    responseText.append(text.trim());
-                }
+                try (BufferedReader read = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream(), "utf-8"))) {
 
-                if (conn.getHeaderField("Content-Type").contains("json")) {
-                    JSONObject jsonObject = new JSONObject(responseText.toString());
-                    System.out.println("JSON:\n" + jsonObject.toString(4));
-                    //JSON:
-                    //{
-                    //    "createdAt": "2023-08-15T18:13:20.881Z",
-                    //    "id": "277",
-                    //    "{'email':'alice@loonycorn.com','firstName':'Alice','lastName':'Alison'}": ""
-                    //}
-                } else {
-                    System.out.println(responseText.toString());
+                    StringBuilder responseText = new StringBuilder();
+
+                    while ((text = read.readLine()) != null) {
+                        responseText.append(text.trim());
+                    }
+
+                    if (conn.getHeaderField("Content-Type").contains("json")) {
+                        JSONObject jsonObj = new JSONObject(responseText.toString());
+                        System.out.println("JSON:\n" + jsonObj.toString(4));
+                    } else {
+                        System.out.println(responseText.toString());
+                    }
                 }
             }
+
+//            try (BufferedReader read = new BufferedReader(
+//                    new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+//
+//                StringBuilder responseText = new StringBuilder();
+//
+//                while ((text = read.readLine()) != null) {
+//                    responseText.append(text.trim());
+//                }
+//                //Exception in thread "main" java.lang.NullPointerException
+//                //at com.loonycorn.SimpleHttpURLConnection.main(SimpleHttpURLConnection.java:53)
+//                if (conn.getHeaderField("Content-Type").contains("json")) {
+//                    JSONObject jsonObject = new JSONObject(responseText.toString());
+//                    System.out.println("JSON:\n" + jsonObject.toString(4));
+//                    //JSON:
+//                    //{"updatedAt": "2023-08-15T19:43:33.223Z"}
+//                } else {
+//                    System.out.println(responseText.toString());
+//                }
+//            }
 
             conn.disconnect();
 
@@ -75,3 +101,5 @@ public class SimpleHttpURLConnection {
 
 // The HttpURLConnection class
 // https://doc.oracle.com/en/java/javase/15/docs/api/java.base/java/net/HttpURLConnection.html
+
+// 204 belongs to the family pf status codes (Success)
